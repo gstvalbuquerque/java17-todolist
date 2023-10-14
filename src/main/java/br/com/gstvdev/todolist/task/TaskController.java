5641191b7e11c8e2fed9a86c2a942bb5d8a144df
gstvalbuquerque <gustavo.sales87@hotmail.com>
2023-10-13 21:22:33 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/gstvdev/todolist/task/TaskController.java
@@ -0,0 +1,70 @@
+package br.com.gstvdev.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gstvdev.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        taskModel.setIdUser((java.util.UUID) request.getAttribute("idUser"));
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Starting date or ending date is invalid. Please, check the dates.");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Starting date is after ending date. Please, check the dates.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.taskRepository.save(taskModel));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TaskModel>> list(HttpServletRequest request) {
+        var idUser = (java.util.UUID) request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser(idUser);
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request,
+            @PathVariable UUID id) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+        }
+
+        if (!task.getIdUser().equals(request.getAttribute("idUser"))) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You are not allowed to update this task");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return ResponseEntity.ok(this.taskRepository.save(task));
+
+    }
+}
